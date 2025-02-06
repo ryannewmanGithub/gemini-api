61a3a667ff93cba80eebe9eeb1d1a74d814cf43a
ryannewmanGithub <RyanNewman280@gmail.com>
2025-02-06 16:18:23 -0500

    Add rudimentary flag parsing to toggle between word-by-word or all at once response

--- script.py
+++ script.py
@@ -3,6 +3,14 @@ import numpy as np
 import sys
 import time
 
+fastflag = False
+
+if len(sys.argv) > 1:
+    flag_arg = sys.argv[1]
+    if flag_arg[0] == '-':
+        if 'q' in flag_arg:
+            fastflag = True
+
 FILENAME = "hiddenfile.txt"
 
 with open(FILENAME, 'r') as f:
@@ -23,27 +31,28 @@ while True:
         model="gemini-2.0-flash",
         contents=userinput,
     )
-
-    #print(response.text)
-
     txt = response.text
-    word_list = txt.split(" ")
-    
-    last = len(word_list)
+    if fastflag:
+        print(txt)
+    else:
+        
+        word_list = txt.split(" ")
+        
+        last = len(word_list)
 
-    for i in range(last):
-        word = word_list[i]
-        if i != last - 1:
-            print(word, end=' ')
-        else:
-            print(word, end='')
-    
-        mean = 0
-        sdev = 0.0001 / 2
-        numsamples = 1
-        noise = np.random.normal(mean, sdev, numsamples)
+        for i in range(last):
+            word = word_list[i]
+            if i != last - 1:
+                print(word, end=' ')
+            else:
+                print(word, end='')
+        
+            mean = 0
+            sdev = 0.0001 / 2
+            numsamples = 1
+            noise = np.random.normal(mean, sdev, numsamples)
 
-        time.sleep(0.0004 + noise[0])
+            time.sleep(0.0004 + noise[0])
     
 
     
